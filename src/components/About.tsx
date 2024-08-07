7225869c6ed8bc8bc49a4ad4123aed433805f039
uche012 <uceenathans@gmail.com>
2024-08-07 13:28:28 +0200

    css changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,11 +29,12 @@ export default function About() {
             </figure>
             <article style={{ height: "auto", width: 500 }}>
               <p>
-                In today's digital landscape, having a strong online presence is
-                crucial for businesses to thrive. Search Engine Optimization
-                (SEO) plays a pivotal role in ensuring your website ranks high
-                on search engine results pages (SERPs), making it more visible
-                to potential customers. Understanding SEO is essential for
+                <span className="serviceheaders">
+                  Search Engine Optimization (SEO)
+                </span>
+                plays a pivotal role in ensuring your website ranks high on
+                search engine results pages (SERPs), making it more visible to
+                potential customers. Understanding SEO is essential for
                 businesses looking to enhance their online visibility and reach
                 their target audience effectively.What is SEO? SEO is the
                 process of optimizing your website's content, structure, and
@@ -41,7 +42,8 @@ export default function About() {
                 Google, Bing, and Yahoo. When users search for specific keywords
                 or phrases related to your business, SEO helps your website
                 appear among the top results, increasing the likelihood of
-                attracting organic traffic.
+                attracting organic traffic.In today's digital landscape, having
+                a strong online presence is crucial for businesses to thrive.
               </p>
             </article>
           </section>
@@ -54,16 +56,17 @@ export default function About() {
           </figure>
           <article style={{ height: "auto", width: 500 }}>
             <p>
-              Web development serves as the backbone of the digital economy,
-              empowering businesses and individuals to leverage the power of the
-              internet to connect, engage, and succeed. By embracing innovative
-              technologies, adopting best practices, and prioritizing user
-              experience, web developers play a pivotal role in shaping the
-              future of digital innovation and driving business growth in the
-              digital age. As businesses continue to embrace digital
-              transformation, investing in professional web development services
-              remains essential for unlocking the full potential of the online
-              marketplace and achieving lasting success.
+              <span className="serviceheaders">Web development</span> serves as
+              the backbone of the digital economy, empowering businesses and
+              individuals to leverage the power of the internet to connect,
+              engage, and succeed. By embracing innovative technologies,
+              adopting best practices, and prioritizing user experience, web
+              developers play a pivotal role in shaping the future of digital
+              innovation and driving business growth in the digital age. As
+              businesses continue to embrace digital transformation, investing
+              in professional web development services remains essential for
+              unlocking the full potential of the online marketplace and
+              achieving lasting success.
             </p>
           </article>
         </section>
@@ -76,7 +79,8 @@ export default function About() {
           </figure>
           <article style={{ height: "auto", width: 500 }}>
             <p>
-              In today's interconnected digital landscape, safeguarding your
+              <span className="serviceheaders">Security</span>is paramount. In
+              today's interconnected digital landscape, safeguarding your
               organization's data, infrastructure, and operations is paramount
               to success and longevity. As a seasoned Systems Administrator with
               a keen focus on security, I am pleased to propose my comprehensive
@@ -94,14 +98,17 @@ export default function About() {
         </figure>
         <article style={{ height: "auto", width: 500 }}>
           <p>
-            Data Analysis and Visualization play a critical role in transforming
-            raw data into actionable insights and compelling narratives. By
-            analyzing and interpreting data trends, patterns, and relationships,
-            organizations can gain valuable insights into customer behavior,
-            market dynamics, and operational performance. Visualization
-            techniques further enhance understanding by presenting complex data
-            in intuitive and visually engaging formats, facilitating
-            decision-making and driving strategic initiatives.
+            <span className="serviceheaders">
+              Data Analysis and Visualization
+            </span>
+            play a critical role in transforming raw data into actionable
+            insights and compelling narratives. By analyzing and interpreting
+            data trends, patterns, and relationships, organizations can gain
+            valuable insights into customer behavior, market dynamics, and
+            operational performance. Visualization techniques further enhance
+            understanding by presenting complex data in intuitive and visually
+            engaging formats, facilitating decision-making and driving strategic
+            initiatives.
           </p>
         </article>
       </section>
